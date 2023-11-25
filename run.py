87b6d48940bfaefb0c13c303c5458c5cee426303
elena5875 <>
2023-11-25 19:58:38 +0000

    Create a function to add stock

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -10,17 +11,30 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('Inventory_of_stocks')
+SHEET = GSPREAD_CLIENT.open('Inventory_of_stocks').sheet1
 
-def get_stocks_in_data():
-    """
-    Get stocks input from the user
-    """
-    print("Please enter stocks for the day.")
-    print("Data should be 6 numbers separated by commas")
-    print("Example: 10,20,30,40,50,60\n")
+#Get the items in the sheet columns
 
-    data_str = input("Enter your data here:")
-    print(f"The data provided is {data_str}")
+products = SHEET.col_values(1)[1:]
 
-get_stocks_in_data()
+
+# Function to display the menu
+def display_menu():
+    print("Menu:")
+    for i, product in enumerate(products, start=1):
+        print(f"{i}. {product}")
+
+# Function to add stock
+def add_stock(product, quantity):
+    # Check if the product is already in the sheet
+    if product in products:
+        # Get the index of the product in the list
+        index = products.index(product) + 1  # Adjusted to start from index 1
+        # Get the current quantity and add the new quantity
+        current_quantity = int(SHEET.cell(index, 2).value)
+        new_quantity = current_quantity + quantity
+        # Update the sheet with the new quantity
+        sheet.update_cell(index, 2, str(new_quantity))
+    else:
+        # If the product is not in the sheet, add a new row
+        sheet.append_row([product, quantity])
