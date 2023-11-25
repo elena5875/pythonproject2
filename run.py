13ec189e4c4d0f39ea48fe7f42fa18fd41b11744
elena5875 <>
2023-11-25 20:05:52 +0000

    created a menu for user to choose

--- run.py
+++ run.py
@@ -29,8 +29,7 @@ def add_stock(product, quantity):
     # Check if the product is already in the sheet
     if product in products:
         # Get the index of the product in the list
-        index = products.index(product) + 1  # Adjusted to start from index 1
-        # Get the current quantity and add the new quantity
+        index = products.index(product) + 1  
         current_quantity = int(SHEET.cell(index, 2).value)
         new_quantity = current_quantity + quantity
         # Update the sheet with the new quantity
@@ -44,8 +43,7 @@ def update_delivery(product, delivered_quantity):
     # Check if the product is in the sheet
     if product in products:
         # Get the index of the product in the list
-        index = products.index(product) + 1  # Adjusted to start from index 1
-        # Get the current quantity and subtract the delivered quantity
+        index = products.index(product) + 1  
         current_quantity = int(sheet.cell(index, 2).value)
         new_quantity = max(current_quantity - delivered_quantity, 0)
         # Update the sheet with the new quantity
@@ -53,14 +51,30 @@ def update_delivery(product, delivered_quantity):
     else:
         print(f"Product '{product}' not found in the inventory.")
 
-# Function to check inventory
+# Function for inventory
 def check_inventory(product):
     # Check if the product is in the sheet
     if product in products:
         # Get the index of the product in the list
-        index = products.index(product) + 1  # Adjusted to start from index 1
-        # Get the current quantity and print it
+        index = products.index(product) + 1  
         current_quantity = int(sheet.cell(index, 2).value)
         print(f"Current inventory of {product}: {current_quantity}")
     else:
         print(f"Product '{product}' not found in the inventory.")
+        
+# Main program
+while True:
+    display_menu()
+    print("Enter the number corresponding to the product (0 to exit): ")
+    choice = input()
+
+    if choice == '0':
+        break
+
+    try:
+        # Convert the choice to an integer and subtract 1 to get the index
+        choice_index = int(choice) - 1
+        selected_product = products[choice_index]
+    except (ValueError, IndexError):
+        print("Invalid choice. Please enter a valid number.")
+        continue
