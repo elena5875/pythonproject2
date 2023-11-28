ae4bce9f890102e5a40700361c34561e38db6c43
elena5875 <>
2023-11-28 20:17:29 +0000

    defining and updating the sheet

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from datetime import date
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -15,40 +16,16 @@ spreadsheet = gspread_client.open('Inventory_of_stocks')
 stocks_in_sheet = spreadsheet.worksheet('stocks_in')
 delivered_sheet = spreadsheet.worksheet('delivered')
 
-
-def update_inventory():
-    # Display menu list from Stocks-In sheet
-    menu_list = stocks_in_sheet.col_values(1)[1:] 
-    print("Menu List:")
-    for i, item in enumerate(menu_list, start=1):
-        print(f"{i}. {item}")
-
-    choice = int(input("Choose a menu item Pleae enter the number: "))
-
-    #for stocks_in quantity
-    stocks_in_quantity = int(input("Please enter the quantity for stocks_in: "))
-
-    # Update Stocks-In sheet
-    stocks_in_cell = stocks_in_sheet.find(menu_list[choice - 1])
-    stocks_in_sheet.update_cell(stocks_in_cell.row, stocks_in_cell.col + 1, stocks_in_quantity)
-
-    #For delivered quantity
-    delivered_quantity = int(input("Please enter the quantity delivered: "))
-    
-    
-
-    # Update Delivered sheet
-    delivered_cell = delivered_sheet.find(menu_list[choice - 1])
-    delivered_sheet.update_cell(delivered_cell.row, delivered_cell.col + 1, delivered_quantity)
-
-    print(f"Stocks-In and Delivered updated for {menu_list[choice - 1]}")
+    #define update sheet
+def update_sheet(sheet, menu_item, quantity_type):
+    quantity_input = int(input(f"Please enter the quantity for {quantity_type} - {menu_item}:"))
+    current_date = date.today().strftime("%Y-%m-%d")
 
 
-update_inventory()
-    
-    
+    cell = sheet.find(menu_item)
+    column_index = len(sheet.row_values(cell.row)) + 1
+    sheet.update_cell(cell.row, column_index, quantity_input)
+    sheet.update_cell(1, column_index, current_date)
 
-
-
-   
+    print(f"{menu_item} updated on {current_date}")
 
