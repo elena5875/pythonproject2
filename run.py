aeab433ea67b376949de2a0b91fbaa371f90d865
elena5875 <>
2023-11-30 21:33:42 +0000

    add a welcome greeting at the beginning of the program

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 from datetime import date
 
+# Set up Google Sheets authentication
 SCOPE = ["https://www.googleapis.com/auth/spreadsheets",
          "https://www.googleapis.com/auth/drive.file",
          "https://www.googleapis.com/auth/drive"]
@@ -12,13 +13,13 @@ spreadsheet = gspread_client.open('Inventory_of_stocks')
 stocks_in_sheet = spreadsheet.worksheet('stocks_in')
 delivered_sheet = spreadsheet.worksheet('stocks_used')
 
-# Add a new sheet called "inventory"
+# Create or get the "inventory" sheet
 try:
     inventory_sheet = spreadsheet.add_worksheet(title='inventory', rows='100', cols='20')
 except gspread.exceptions.APIError:
-    # If the sheet already exists, use the existing sheet
     inventory_sheet = spreadsheet.worksheet('inventory')
 
+# Function to update a sheet with quantity information
 def update_sheet(sheet, menu_item, quantity_type):
     try:
         cell = sheet.find(menu_item)
@@ -36,16 +37,25 @@ def update_sheet(sheet, menu_item, quantity_type):
 
     print(f"{menu_item} updated on {current_date}")
 
-# Display the menu list
-menu_list = stocks_in_sheet.col_values(1)[1:]
+# Display a welcome message
+print("Welcome to the Inventory Management System!")
+print("This will allow you to update and manage the inventory of stocks in your pantry.")
 
+# Main loop for user interaction
 while True:
     print("Menu List:")
+    menu_list = stocks_in_sheet.col_values(1)[1:]
+
     for i, item in enumerate(menu_list, start=1):
         print(f"{i}. {item}")
+    print(f"{len(menu_list) + 1}. Exit")
 
-    # Ask the user to choose a menu item
     choice = int(input("Choose a menu item (Enter the number):"))
+
+    # Check if the user chose the "Exit" option
+    if choice == len(menu_list) + 1:
+        print("Exiting the Inventory Management System. Goodbye!")
+        break
 
     # Update "stocks_in" and "stocks_used" sheets based on the user's choice
     if 1 <= choice <= len(menu_list):
@@ -60,10 +70,9 @@ while True:
         # Calculate the difference and update the "inventory" sheet
         inventory_values = []
         for i, item in enumerate(menu_list, start=1):
-            stocks_in_value = stocks_in_sheet.cell(i + 1, 2).value  # Assuming quantity is in column 2
-            stocks_used_value = delivered_sheet.cell(i + 1, 2).value  # Assuming quantity is in column 2
+            stocks_in_value = stocks_in_sheet.cell(i + 1, 2).value
+            stocks_used_value = delivered_sheet.cell(i + 1, 2).value
 
-            # Check if either value is None, and handle it accordingly
             if stocks_in_value is None:
                 stocks_in_value = 0
             if stocks_used_value is None:
@@ -72,10 +81,7 @@ while True:
             difference = int(stocks_in_value) - int(stocks_used_value)
             inventory_values.append([item, difference])
 
-        # Clear the existing data in the "inventory" sheet
         inventory_sheet.clear()
-
-        # Update the "inventory" sheet with the calculated values
         inventory_sheet.append_rows([["Quatntity left in the stockroom"]] + inventory_values)
 
         # Display the inventory list
@@ -83,7 +89,6 @@ while True:
         print("\nInventory List:")
         for row in inventory_list:
             print(row)
-        break  
-
+        break
 
 
