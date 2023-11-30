696a1bc8bbd6ede9f51804e9bf354a7ae44e8d25
elena5875 <>
2023-11-30 19:00:48 +0000

    update sheet function. Add a new inventory sheet

--- run.py
+++ run.py
@@ -2,6 +2,8 @@ import gspread
 from google.oauth2.service_account import Credentials
 from datetime import date
 
+
+    # Google Sheet API
 SCOPE = ["https://www.googleapis.com/auth/spreadsheets",
          "https://www.googleapis.com/auth/drive.file",
          "https://www.googleapis.com/auth/drive"]
@@ -10,45 +12,16 @@ scoped_credentials = CREDS.with_scopes(SCOPE)
 gspread_client = gspread.authorize(scoped_credentials)
 spreadsheet = gspread_client.open('Inventory_of_stocks')
 stocks_in_sheet = spreadsheet.worksheet('stocks_in')
-delivered_sheet = spreadsheet.worksheet('stocks_used')
+stocks_used_sheet = spreadsheet.worksheet('stocks_used')
+inventory_sheet = spreadsheet.worksheet('inventory')
 
-# Add a new sheet called "inventory"
-try:
-    inventory_sheet = spreadsheet.add_worksheet(title='inventory', rows='100', cols='20')
-except gspread.exceptions.APIError:
-    # If the sheet already exists, use the existing sheet
-    inventory_sheet = spreadsheet.worksheet('inventory')
-
-def update_sheet(sheet, menu_item, quantity_type):
-    try:
-        cell = sheet.find(menu_item)
-    except gspread.exceptions.CellNotFound:
-        sheet.append_row([menu_item, 0])
-        cell = sheet.find(menu_item)
-
-    column_index = len(sheet.row_values(cell.row)) + 1
-
-    quantity_input = int(input(f"Enter {quantity_type} quantity for {menu_item}:"))
+    #Update sheet function
+def update_sheet(sheet, menu_item, quantity_type, quantity_input):
     current_date = date.today().strftime("%Y-%m-%d")
 
-    sheet.update_cell(cell.row, column_index, quantity_input)
-    sheet.update_cell(1, column_index, current_date)
+    cell = sheet.find(menu_item)
+    col_index = len(sheet.row_values(cell.row)) + 1
+    sheet.update_cell(cell.row, col_index, quantity_input)
+    sheet.update_cell(1, col_index, current_date)
 
-    print(f"{menu_item} updated on {current_date}")
-
-# Display the menu list
-menu_list = stocks_in_sheet.col_values(1)[1:]
-print("Menu List:")
-for i, item in enumerate(menu_list, start=1):
-    print(f"{i}. {item}")
-
-# Ask the user to choose a menu item
-choice = int(input("Choose a menu item (Enter the number):"))
-
-# Update all three sheets based on the user's choice
-if 1 <= choice <= len(menu_list):
-    update_sheet(stocks_in_sheet, menu_list[choice - 1], "stocks_in")
-    update_sheet(delivered_sheet, menu_list[choice - 1], "stocks_used")
-    update_sheet(inventory_sheet, menu_list[choice - 1], "inventory")
-else:
-    print("Invalid choice. Enter a valid menu item number.")
+    print(f"{menu_item} {quantity_type} updated on {current_date}")
