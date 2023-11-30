4e7bccf6c33c0ac1e159b7cc472aa87ef05bd62f
elena5875 <>
2023-11-30 21:03:14 +0000

    add a function and a loop where the user is asked if it wants to continue or not

--- run.py
+++ run.py
@@ -27,58 +27,44 @@ def update_sheet(sheet, menu_item, quantity_input, quantity_type):
 # Create a menu list from "stocks_in" sheet
 menu_list = stocks_in_sheet.col_values(1)[1:]
 
-# Display the menu to the user
-print("Menu List:")
-for index, item in enumerate(menu_list, start=1):
-    print(f"{index}. {item}")
-
-# Add an option to exit
-print(f"{len(menu_list) + 1}. Exit")
-
-# Get user input for stocks_in
 while True:
+    print("Menu List:")
+    for index, item in enumerate(menu_list, start=1):
+        print(f"{index}. {item}")
+
+    print(f"{len(menu_list) + 1}. Exit")
+
     try:
         user_choice = int(input("Choose a menu item (enter the number): "))
-        
+
         if 1 <= user_choice <= len(menu_list):
             chosen_item = menu_list[user_choice - 1]
             print(f"You've chosen: {chosen_item}")
 
-            # Prompt the user for quantity input for stocks_in
-            while True:
-                quantity_input = input(f"Enter quantity for {chosen_item} (stocks_in): ")
+            for sheet, prompt in [(stocks_in_sheet, "stocks_in"), (stocks_used_sheet, "stocks_used")]:
+                quantity_input = input(f"Enter quantity for {chosen_item} ({prompt}): ")
 
-                # Validate the quantity input
-                if quantity_input.isdigit():
-                    quantity_input = int(quantity_input)
-                    update_sheet(stocks_in_sheet, chosen_item, quantity_input, "Quantity Type")
-                    break
-                else:
-                    print("Invalid quantity input. Please enter a valid integer.")
-            break
+                while not quantity_input.isdigit():
+                    print("Invalid input. Please enter a valid number.")
+                    quantity_input = input(f"Enter quantity for {chosen_item} ({prompt}): ")
+
+                quantity_input = int(quantity_input)
+                update_sheet(sheet, chosen_item, quantity_input, f"Quantity {prompt.capitalize()}")
+
+            continue_choice = input("Do you want to continue? (yes/no): ").lower()
+
+            if continue_choice == 'no':
+                print("Exiting the program. Goodbye!")
+                exit()
+
         elif user_choice == len(menu_list) + 1:
             print("Exiting the program. Goodbye!")
             exit()
+
         else:
             print("Invalid choice. Please enter a valid number.")
+
     except ValueError:
         print("Invalid input. Please enter a number.")
 
-# Get user input for stocks_used
-while True:
-    used_menu_list = stocks_used_sheet.col_values(1)[1:]
-    used_chosen_item = menu_list[user_choice - 1]  # using the same chosen_item for simplicity
-
-    # Prompt the user for quantity input for stocks_used
-    while True:
-        used_quantity_input = input(f"Enter quantity for {used_chosen_item} (stocks_used): ")
-
-        # Validate the quantity input
-        if used_quantity_input.isdigit():
-            used_quantity_input = int(used_quantity_input)
-            update_sheet(stocks_used_sheet, used_chosen_item, used_quantity_input, "Quantity Used")
-            break
-        else:
-            print("Invalid quantity input. Please enter a valid integer.")
-    break
 
